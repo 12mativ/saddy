c1635e1c5bee99a7e3a18f956cf5f1cbb658946a
12mativ <74101501+12mativ@users.noreply.github.com>
2022-06-17 16:06:13 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+// while (true) {
+//     let answer = prompt('Грустишь?');
+//     if (answer.toLowerCase() === 'да') {
+//         alert('Не грусти!!!');
+//         break
+//     } else if (answer.toLowerCase() === 'нет') {
+//         alert('Умница!');
+//         break
+//     } else {
+//         alert('Не понял тебя, попробуй заново.')
+//     }
+
+// }
